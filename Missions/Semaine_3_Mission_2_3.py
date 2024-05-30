e79493d40da7db95a4cef7ca93cddeb90bb0f7d5
FabriceDavidGit <david@lyceejulesfil.fr>
2024-05-30 21:31:00 +0000

    Commit

--- Missions/Semaine_3_Mission_2_3.py
+++ Missions/Semaine_3_Mission_2_3.py
@@ -1,4 +1,6 @@
 from pyModbusTCP.client import ModbusClient
+from import_ccwmod_manager import ImportCCWManager
+
 import time
 
 def convert_bits_int(value_bits):
@@ -52,6 +54,13 @@ def convert_32bits_reel(value_32bits):
  value = round(value,2)
  return value
 
+# Création d'un Objet de la Classe ImportCCWManager
+import_ccw_manager = ImportCCWManager()
+
+# Récupération des Variables, des Adresses Modbus et de leur Type
+list_address_modbus = import_ccw_manager.return_list_address_modbus()
+print(list_address_modbus)
+
 
 
 # Accès à l'Automate par ModBus TCP
@@ -69,109 +78,200 @@ try:
 
     # Registre en Liste de Booléen de 1 sauf Mention
 
-    capt_bas_moulin = c.read_coils(0, 1)
+
+
+    liste_address_modbus1 = list_address_modbus[0]
+    address = int(liste_address_modbus1.get("address")) - 1
+    capt_bas_moulin = c.read_coils(address, 1)
 
     print ("Type des Variables capt_bas_moulin : ",type(capt_bas_moulin),"\n")
     print ("capt_bas_moulin : ",capt_bas_moulin,"\n")
-    sign_ext_son = c.read_coils(1, 1)
+
+    liste_address_modbus1 = list_address_modbus[1]
+    address = int(liste_address_modbus1.get("address")) - 1
+    sign_ext_son = c.read_coils(address, 1)
     sign_ext_son = sign_ext_son[0]
-    capt_rot_blute = c.read_coils(2, 1)
+
+    liste_address_modbus1 = list_address_modbus[2]
+    address = int(liste_address_modbus1.get("address")) - 1
+    capt_rot_blute = c.read_coils(address, 1)
     capt_rot_blute = capt_rot_blute[0]
-    inter_shunt_secu = c.read_coils(3, 1)
+
+    liste_address_modbus1 = list_address_modbus[3]
+    address = int(liste_address_modbus1.get("address")) - 1
+    inter_shunt_secu = c.read_coils(address, 1)
     inter_shunt_secu = inter_shunt_secu[0]
-    capt_ensach = c.read_coils(4, 1)
+
+    liste_address_modbus1 = list_address_modbus[4]
+    address = int(liste_address_modbus1.get("address")) - 1
+    capt_ensach = c.read_coils(address, 1)
     capt_ensach = capt_ensach[0]
-    run_powerflex = c.read_coils(5, 1)
+
+    liste_address_modbus1 = list_address_modbus[5]
+    address = int(liste_address_modbus1.get("address")) - 1
+    run_powerflex = c.read_coils(address, 1)
     run_powerflex = run_powerflex[0]
-    moteur_succion = c.read_coils(6, 1)
+
+    liste_address_modbus1 = list_address_modbus[6]
+    address = int(liste_address_modbus1.get("address")) - 1
+    moteur_succion = c.read_coils(address, 1)
     moteur_succion = moteur_succion[0]
-    moteur_son = c.read_coils(7, 1)
+
+    liste_address_modbus1 = list_address_modbus[7]
+    address = int(liste_address_modbus1.get("address")) - 1
+    moteur_son = c.read_coils(address, 1)
     moteur_son = moteur_son[0]
-    battage = c.read_coils(8, 1)
+
+
+    liste_address_modbus1 = list_address_modbus[8]
+    address = int(liste_address_modbus1.get("address")) - 1
+    battage = c.read_coils(address, 1)
     battage = battage[0]
-    out_sign_son = c.read_coils(9, 1)
+
+
+
+    liste_address_modbus1 = list_address_modbus[9]
+    address = int(liste_address_modbus1.get("address")) - 1
+    out_sign_son = c.read_coils(address, 1)
     out_sign_son = out_sign_son[0]
-    moteur_bluterie = c.read_coils(10, 1)
+
+
+
+    liste_address_modbus1 = list_address_modbus[10]
+    address = int(liste_address_modbus1.get("address")) - 1
+    moteur_bluterie = c.read_coils(address, 1)
     moteur_bluterie = moteur_bluterie[0]
 
+
+
     # Courant Meule Mesurée en Réel sur 32 Bits
-    courant_meule_mesuree = c.read_coils(11, 32)
+    liste_address_modbus1 = list_address_modbus[11]
+    address = int(liste_address_modbus1.get("address")) - 1
+    courant_meule_mesuree = c.read_coils(address, 32)
     courant_meule_mesuree = convert_32bits_reel(courant_meule_mesuree)
 
-    stop_powerflex_true = c.read_coils(43, 1)
+
+    liste_address_modbus1 = list_address_modbus[12]
+    address = int(liste_address_modbus1.get("address")) - 1
+    stop_powerflex_true = c.read_coils(address, 1)
     stop_powerflex_true = stop_powerflex_true[0]
 
     # Stop PowerFlex en Booléen sur 5 Bits
-    stop_powerflex = c.read_coils(44, 5)
+    liste_address_modbus1 = list_address_modbus[13]
+    address = int(liste_address_modbus1.get("address")) - 1
+    stop_powerflex = c.read_coils(address, 5)
     stop_powerflex_4 = int(stop_powerflex[4])
 
-    start_powerflex = c.read_coils(49, 1)
+
+
+    liste_address_modbus1 = list_address_modbus[14]
+    address = int(liste_address_modbus1.get("address")) - 1
+    start_powerflex = c.read_coils(address, 1)
     start_powerflex = int(start_powerflex[0])
 
     # Start Bluterie en Booléen sur 2 Bits
-    start_bluterie = c.read_coils(50, 2)
+    liste_address_modbus1 = list_address_modbus[15]
+    address = int(liste_address_modbus1.get("address")) - 1
+    start_bluterie = c.read_coils(address, 2)
     start_bluterie_0 = int(start_bluterie[0])
 
     # Current Meule Mesurée en Réel sur 32 Bits
-    current_meule_mesuree = c.read_coils(52, 32)
+    liste_address_modbus1 = list_address_modbus[16]
+    address = int(liste_address_modbus1.get("address")) - 1
+    current_meule_mesuree = c.read_coils(address, 32)
     current_meule_mesuree = convert_32bits_reel(current_meule_mesuree)
 
 
     # Température Meule en INT sur 16 Bits
-    temperature_meule = c.read_coils(84, 16)
+    liste_address_modbus1 = list_address_modbus[17]
+    address = int(liste_address_modbus1.get("address")) - 1
+    temperature_meule = c.read_coils(address, 16)
     temperature_meule = convert_bits_int(temperature_meule)
 
-    capt_rot_bluterie_0 = c.read_coils(100, 1)
+    liste_address_modbus1 = list_address_modbus[18]
+    address = int(liste_address_modbus1.get("address")) - 1
+    capt_rot_bluterie_0 = c.read_coils(address, 1)
     capt_rot_bluterie_0 = int(capt_rot_bluterie_0[0])
-    alarm_entretien = c.read_coils(101, 1)
+
+    liste_address_modbus1 = list_address_modbus[19]
+    address = int(liste_address_modbus1.get("address")) - 1
+    alarm_entretien = c.read_coils(address, 1)
     alarm_entretien = int(alarm_entretien[0])
 
     # Horamètre en DINT sur 32 Bits
-    horametre = c.read_coils(102, 32)
+    liste_address_modbus1 = list_address_modbus[20]
+    address = int(liste_address_modbus1.get("address")) - 1
+    horametre = c.read_coils(address, 32)
     horametre = convert_bits_int(horametre)
 
     # Retour Fréquence Meule en Réel sur 32 Bits
-    retour_frequence_meule = c.read_coils(134, 32)
+    liste_address_modbus1 = list_address_modbus[21]
+    address = int(liste_address_modbus1.get("address")) - 1
+    retour_frequence_meule = c.read_coils(address, 32)
     retour_frequence_meule = convert_32bits_reel(retour_frequence_meule)
 
-    stop_powerflex_vibrat = c.read_coils(166, 1)
+
+    liste_address_modbus1 = list_address_modbus[22]
+    address = int(liste_address_modbus1.get("address")) - 1
+    stop_powerflex_vibrat = c.read_coils(address, 1)
     stop_powerflex_vibrat = int(stop_powerflex_vibrat[0])
 
     # Réglage Intensité Meule en Réel sur 32 Bits
-    reglage_intensite_meule = c.read_coils(167, 32)
+    liste_address_modbus1 = list_address_modbus[23]
+    address = int(liste_address_modbus1.get("address")) - 1
+    reglage_intensite_meule = c.read_coils(address, 32)
     reglage_intensite_meule = convert_32bits_reel(reglage_intensite_meule)
 
-    capt_ensach_alarme = c.read_coils(199, 1)
+    liste_address_modbus1 = list_address_modbus[24]
+    address = int(liste_address_modbus1.get("address")) - 1
+    capt_ensach_alarme = c.read_coils(address, 1)
     capt_ensach_alarme = int(capt_ensach_alarme[0])
 
-    capt_bas_alarme = c.read_coils(200, 1)
+    liste_address_modbus1 = list_address_modbus[25]
+    address = int(liste_address_modbus1.get("address")) - 1
+    capt_bas_alarme = c.read_coils(address, 1)
     capt_bas_alarme = int(capt_bas_alarme[0])
 
-    intensite_meule = c.read_coils(201, 1)
+    liste_address_modbus1 = list_address_modbus[26]
+    address = int(liste_address_modbus1.get("address")) - 1
+    intensite_meule = c.read_coils(address, 1)
     intensite_meule = int(intensite_meule[0])
 
-    arret_vibra = c.read_coils(202, 1)
+    liste_address_modbus1 = list_address_modbus[27]
+    address = int(liste_address_modbus1.get("address")) - 1
+    arret_vibra = c.read_coils(address, 1)
     arret_vibra = int(arret_vibra[0])
 
-    succion_force = c.read_coils(203, 1)
+    liste_address_modbus1 = list_address_modbus[28]
+    address = int(liste_address_modbus1.get("address")) - 1
+    succion_force = c.read_coils(address, 1)
     succion_force = int(succion_force[0])
 
-    vibra_eleve = c.read_coils(204, 1)
+    liste_address_modbus1 = list_address_modbus[29]
+    address = int(liste_address_modbus1.get("address")) - 1
+    vibra_eleve = c.read_coils(address, 1)
     vibra_eleve = int(vibra_eleve[0])
 
-    vidan_trem = c.read_coils(205, 1)
+    liste_address_modbus1 = list_address_modbus[30]
+    address = int(liste_address_modbus1.get("address")) - 1
+    vidan_trem = c.read_coils(address, 1)
     vidan_trem = int(vidan_trem[0])
 
     # Vitesse Vibrateur en Réel sur 32 Bits
-    speed_ref_vibrat = c.read_coils(206, 32)
+    liste_address_modbus1 = list_address_modbus[31]
+    address = int(liste_address_modbus1.get("address")) - 1
+    speed_ref_vibrat = c.read_coils(address, 32)
   
     # speed_ref_vibrat = convert_32bits_reel(speed_ref_vibrat)
-    start_powerflex_vibrat = c.read_coils(238, 1)
+    liste_address_modbus1 = list_address_modbus[32]
+    address = int(liste_address_modbus1.get("address")) - 1
+    start_powerflex_vibrat = c.read_coils(address, 1)
   
     # Consigne Vibrateur en Réel sur 32 Bits
-    cv_manuel = c.read_coils(239, 32)
+    liste_address_modbus1 = list_address_modbus[33]
+    address = int(liste_address_modbus1.get("address")) - 1
+    cv_manuel = c.read_coils(address, 32)
   
-
     # Affichage
     print ("Etat Capteur Bas Moulin : ",capt_bas_moulin)
     print ("Sign Ext Son : ",sign_ext_son)
